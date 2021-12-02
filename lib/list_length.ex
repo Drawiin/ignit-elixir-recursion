91e44b75031de30720d2ab30877f2ae86c2902f6
idd_vguimaraes <idd_guimaraes@uolinc.com>
2021-12-02 00:23:13 -0400

    challenge completed

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,14 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list) do
+    count(list, 0)
+  end
+
+  defp count([_ | rest], size) do
+    size = size + 1
+    count(rest, size)
+  end
+
+  defp count([], size) do
+    size
   end
 end
